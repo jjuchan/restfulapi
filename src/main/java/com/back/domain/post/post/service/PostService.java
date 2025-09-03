5fced4fde4e9eb7a7431eea028bca6e14fd32ed0
jjuchan <yjc7241@naver.com>
2025-09-03 12:17:46 +0900

    feat:postComment add

--- src/main/java/com/back/domain/post/post/service/PostService.java
+++ src/main/java/com/back/domain/post/post/service/PostService.java
@@ -2,6 +2,7 @@ package com.back.domain.post.post.service;
 
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.repository.PostRepository;
+import com.back.domain.post.postComment.entity.PostComment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -34,5 +35,17 @@ public class PostService {
         return postRepository.findById(id).orElseThrow(
                 () -> new RuntimeException("게시글이 존재하지 않습니다.")
         );
+    }
+
+    public void createComment(Post post, String content) {
+        post.addComment(content);
+    }
+
+    public boolean deleteComment(Post post, PostComment postComment) {
+        return post.deleteComment(postComment);
+    }
+
+    public void modify(PostComment postComment, String content) {
+        postComment.modify(content);
     }
 }
