99a181f93cb22609c6033f1257e0434379b7a1d5
jjuchan <yjc7241@naver.com>
2025-09-04 09:37:36 +0900

    refactor:transactional 설정 추가 후 delete add

--- src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -5,6 +5,7 @@ import com.back.domain.post.post.service.PostService;
 import com.back.domain.post.postComment.dto.PostCommentDto;
 import com.back.domain.post.postComment.entity.PostComment;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +19,12 @@ import java.util.List;
 public class ApiV1PostCommentController {
     private final PostService postService;
 
+    @Transactional(readOnly = true)
     @GetMapping
     public List<PostCommentDto> getItems(
             @PathVariable long postId
     ) {
-        Post post = postService.getPost(postId);
+        Post post = postService.findById(postId);
 
         return post
                 .getComments()
@@ -31,15 +33,31 @@ public class ApiV1PostCommentController {
                 .toList();
     }
 
+    @Transactional(readOnly = true)
     @GetMapping("/{id}")
     public PostCommentDto getItem(
             @PathVariable long postId,
             @PathVariable long id
     ) {
-        Post post = postService.getPost(postId);
+        Post post = postService.findById(postId);
 
         PostComment postComment = post.findCommentById(id).get();
 
         return new PostCommentDto(postComment);
+    }
+
+    @Transactional
+    @GetMapping("/{id}/delete")
+    public String delete(
+            @PathVariable long postId,
+            @PathVariable long id
+    ) {
+        Post post = postService.findById(postId);
+
+        PostComment postComment = post.findCommentById(id).get();
+
+        postService.deleteComment(post, postComment);
+
+        return "%d 댓글이 삭제 되었습니다.".formatted(id);
     }
 }
