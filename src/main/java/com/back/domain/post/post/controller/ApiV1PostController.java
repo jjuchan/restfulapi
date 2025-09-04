d93b5e829450d8ea9f5f7d2bc2b2f8aee6cea054
jjuchan <yjc7241@naver.com>
2025-09-04 12:42:54 +0900

    refactor: requestDto add

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -1,9 +1,11 @@
 package com.back.domain.post.post.controller;
 
 import com.back.domain.post.post.dto.PostDto;
+import com.back.domain.post.post.dto.PostWriteReqbody;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import com.back.global.rsData.RsData;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -38,10 +40,24 @@ public class ApiV1PostController {
     @Transactional
     @DeleteMapping("/{id}")
     public RsData<PostDto> delete(@PathVariable Long id) {
-        Post item = postService.findById(id);
+        Post post = postService.findById(id);
 
-        postService.delete(item);
+        postService.delete(post);
 
-        return new RsData<>("200-1", "%d번 게시글이 삭제되었습니다.".formatted(id), new PostDto(item));
+        return new RsData<>("200-1", "%d번 게시글이 삭제되었습니다.".formatted(id), new PostDto(post));
+    }
+
+
+
+    @PostMapping
+    @Transactional
+    public RsData<PostDto> write(@Valid @RequestBody PostWriteReqbody form) {
+        Post post = postService.create(form.title(), form.content());
+
+        return new RsData<>(
+                "200-1",
+                "%d번 게시글이 생성되었습니다.".formatted(post.getId()),
+                new PostDto(post)
+        );
     }
 }
---
+++ src/main/java/com/back/domain/post/post/dto/PostWriteReqbody.java
@@ -0,0 +1,13 @@
+package com.back.domain.post.post.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record PostWriteReqbody(
+        @NotBlank
+        @Size(min = 2, max = 100)
+        String title,
+        @NotBlank
+        @Size(min = 2, max = 100)
+        String content
+){}
