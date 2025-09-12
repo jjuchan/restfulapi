96084ab1b25f65c94e43cc8d947c2c7eeb7d4744
jjuchan <yjc7241@naver.com>
2025-09-12 10:34:54 +0900

    refactor: router add

--- p-14533-1/src/app/posts/write/page.tsx
+++ p-14533-1/src/app/posts/write/page.tsx
@@ -39,7 +39,7 @@ export default function Page() {
             }),
         }).then((data) => {
             alert(data.msg)
-            router.push(`/posts/${data.data.id}`)
+            router.replace(`/posts/${data.data.id}`)
         })
     }
 
