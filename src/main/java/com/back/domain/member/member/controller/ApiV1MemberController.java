b39c46aea055f2895a7b3983ff29dd62c113824f
jjuchan <yjc7241@naver.com>
2025-09-15 15:19:12 +0900

    feat:login, join add

---
+++ src/main/java/com/back/domain/member/member/controller/ApiV1MemberController.java
@@ -0,0 +1,56 @@
+package com.back.domain.member.member.controller;
+
+import com.back.domain.member.member.dto.MemberDto;
+import com.back.domain.member.member.dto.MemberJoinReqBody;
+import com.back.domain.member.member.dto.MemberLoginReqBody;
+import com.back.domain.member.member.dto.MemberLoginResBody;
+import com.back.domain.member.member.entity.Member;
+import com.back.domain.member.member.service.MemberService;
+import com.back.global.exception.ServiceException;
+import com.back.global.rsData.RsData;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/members")
+@RequiredArgsConstructor
+@Tag(name="ApiV1MemberController", description = "API 맴버 컨트롤러")
+public class ApiV1MemberController {
+    private final MemberService memberService;
+
+    @PostMapping
+    public RsData<MemberDto> join(@Valid @RequestBody MemberJoinReqBody reqBody) {
+        Member member = memberService.join(reqBody.username(), reqBody.password(), reqBody.nickname());
+
+        return new RsData<>(
+                "201-1",
+                "%s님 환영합니다. 회원가입이 완료되었습니다.".formatted(member.getNickname()),
+                new MemberDto(member)
+        );
+    }
+
+    @PostMapping("/login")
+    public RsData<MemberLoginResBody> login(@Valid @RequestBody MemberLoginReqBody reqBody) {
+        Member member = memberService.findByUsername(reqBody.username())
+                .orElseThrow(() -> new ServiceException("401-1", "존재하지 않는 회원입니다."));
+
+        if (!member.getPassword().equals(reqBody.password())) {
+            throw new ServiceException("401-2", "비밀번호가 일치하지 않습니다.");
+        }
+
+
+        return new RsData<>(
+                "200-1",
+                "%s님 환영합니다.".formatted(member.getNickname()),
+                new MemberLoginResBody(
+                        new MemberDto(member),
+                        member.getApiKey())
+        );
+
+    }
+}
