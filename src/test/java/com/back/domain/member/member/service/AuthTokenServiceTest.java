64253985f3bb393bcbc49871dfa19be36094d2dd
jjuchan <yjc7241@naver.com>
2025-09-18 15:50:55 +0900

    refactor: screat key yml add

--- src/test/java/com/back/domain/member/member/service/AuthTokenServiceTest.java
+++ src/test/java/com/back/domain/member/member/service/AuthTokenServiceTest.java
@@ -1,10 +1,13 @@
 package com.back.domain.member.member.service;
 
+import com.back.domain.member.member.entity.Member;
+import com.back.global.standard.util.Ut;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,7 +17,8 @@ import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.Map;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -22,6 +26,15 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class AuthTokenServiceTest {
     @Autowired
     private AuthTokenService authTokenService;
+
+    @Autowired
+    private MemberService memberService;
+    @Value("${custom.jwt.secretKey}")
+    private String jwtSecretKey;
+
+    @Value("${custom.accessToken.expireSeconds}")
+    private int accessTokenExpireSeconds;
+
 
     @Test
     @DisplayName("authTokenService가 존재한다.")
@@ -32,8 +45,7 @@ public class AuthTokenServiceTest {
     @Test
     @DisplayName("jjwt 최신 방식으로 jwt 생성, {name= \"Paul\", age=23}")
     void t2 () {
-        // 서명키 (SecretKey)
-        String jwtSecretKey = "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890";
+
         byte[] keyBytes = jwtSecretKey.getBytes(StandardCharsets.UTF_8);
         SecretKey secretKey = Keys.hmacShaKeyFor(keyBytes);
 
@@ -41,7 +53,7 @@ public class AuthTokenServiceTest {
         Map<String, Object> claims = Map.of("name", "Paul", "age", "23");
 
         // 생성시간, 만료시간 설정
-        long expireMillis = 1000L * 60 * 60 * 24 * 365; // 토큰 만료시간을 1년
+        long expireMillis = 1000L * accessTokenExpireSeconds; // 토큰 만료시간을 1년
         Date issuedAt = new Date(); // 발행시간 설정
         Date expiration = new Date(issuedAt.getTime() + expireMillis); //발행 시간으로 부터 만료시간 설정
 
@@ -55,5 +67,50 @@ public class AuthTokenServiceTest {
         assertThat(jwt).isNotBlank();
 
         System.out.println("jwt : " + jwt);
+
+        assertThat(Ut.jwt.isValid(jwtSecretKey, jwt)).isTrue();
+        Map<String, Object> parsedPayload = Ut.jwt.payload(jwtSecretKey, jwt);
+        assertThat(parsedPayload).containsAllEntriesOf(claims);
+    }
+
+    @Test
+    @DisplayName("Ut.jwt.toString 통해서 jwt 생성, {name= \"Paul\", age=23}")
+    void t3 () {
+        Map<String, Object> claims = Map.of("name", "David", "age", "20");
+
+        String jwt = Ut.jwt.toString(
+                jwtSecretKey,
+                accessTokenExpireSeconds,
+                claims
+        );
+
+        assertThat(jwt).isNotBlank();
+
+        System.out.println("jwt : " + jwt);
+
+        assertThat(Ut.jwt.isValid(jwtSecretKey, jwt)).isTrue();
+        Map<String, Object> parsedPayload = Ut.jwt.payload(jwtSecretKey, jwt);
+        assertThat(parsedPayload).containsAllEntriesOf(claims);
+    }
+
+    @Test
+    @DisplayName("authTokenService.genAccessToken(member);")
+    void t4 () {
+        Member member = memberService.findByUsername("user1").get();
+        String accessToken = authTokenService.genAccessToken(member);
+
+        assertThat(accessToken).isNotBlank();
+
+        System.out.println("accessToken : " + accessToken);
+
+        Map<String, Object> parsedPayload = authTokenService.payload(accessToken);
+
+        assertThat(parsedPayload)
+                .containsAllEntriesOf(
+                        Map.of(
+                                "id", member.getId(),
+                                "username", member.getUsername()
+                        )
+                );
     }
 }
